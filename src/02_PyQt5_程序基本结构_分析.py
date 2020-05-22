f0e8c4ecb5ada0d071b1fe5d4fb467095b343ce3
Nick <pengyang19@dingtalk.com>
2020-05-23 00:59:27 +0800

    程序基本结构分析

--- src/02_PyQt5_程序基本结构_分析.py
+++ src/02_PyQt5_程序基本结构_分析.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # @Author      : Nick
-# @File        : 02_PyQt5_程序基本机构分析.py  v1.0
+# @File        : 02_PyQt5_程序基本结构_分析.py  v1.0
 # @Contact     : PENGYANG19@163.COM
 # @CreateTime  : 2020-05-22 18:09:33
 # Copyright (C) 2020 Nick Ltd. All rights reserved.
