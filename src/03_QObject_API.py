60cc96b0c08d133bb30e31b03e3ca71ea0f0005e
Nick <pengyang19@dingtalk.com>
2020-05-27 17:10:24 +0800

    QObject对象名称和属性的操作

--- src/03_QObject_API.py
+++ src/03_QObject_API.py
@@ -20,6 +20,7 @@ class Window(QWidget):
         :return:
         """
         self.get_Qobject_parent_cls()
+        self.operate_objName_property()
 
     def get_Qobject_parent_cls(self):
         """
@@ -29,6 +30,21 @@ class Window(QWidget):
         # QObject.__subclasses__()    # 获取QObject的子类
         mro_list = QObject.mro()
         print(mro_list)
+
+    def operate_objName_property(self):
+        """
+        QObject对象名称和属性的操作
+        :return:
+        """
+        # 测试API
+        obj = QObject()
+        obj.setObjectName("notice")     # 给一个Qt对象设置一个名称, 一般这个名称是唯一的, 当做对象的ID来用
+        print('obj设置的对象名称为: ', obj.objectName())   # 获取一个Qt对象的名称
+
+        obj.setProperty("notice_level1", "error")   # 给一个Qt对象动态添加一个属性和值
+        obj.setProperty("notice_level2", "warning")
+        print("获取对象obj设置属性notice_level1的值为: ", obj.property("notice_level1"))   # 获取对象obj的属性值
+        print("获取对象obj所有设置的属性名称: ", obj.dynamicPropertyNames())     # 获取对象obj中所有通过setProperty设置的属性名称
 
 
 if __name__ == '__main__':
